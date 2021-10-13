97f1534d64f68e288af157c0407b891099e9dc65
Andreas Proteus <proteuss@sdf.lonestar.org>
2021-10-13 11:00:57 +0300

    Fix some missing lemata

--- phot-30.pl
+++ phot-30.pl
@@ -7,8 +7,8 @@
 #
 #  DESCRIPTION: Converts utf-8 Photius lexicon Volume II from TLG to dsl format
 #
-#       AUTHOR: proteus (), proteuss@sdf.org
-#      VERSION: 1.0
+#      AUTHOR: proteus (), proteuss@sdf.org
+#      VERSION: 1.3
 #      CREATED: 10/01/2021
 #===============================================================================
 
@@ -22,20 +22,20 @@ binmode STDOUT, ":utf8";
 my $phot_file = shift @ARGV;
 my $phot_text = read_file($phot_file, encoding => 'utf8' );
 
-my @lemas = split(/</, $phot_text);    # Split on '<>:'
+my @lemas = split(/</, $phot_text);      # Split on '<>:'
 shift @lemas;
 foreach my $lema (@lemas){
-  next if $lema =~ /(\{Ἀρχὴ σὺν θεῷ)|(\{Τέλος)/; # Not letter header lines
+  $lema =~s/\{.*?\}//g;                  # Titles that do not belong to lemata
   my ($head, $body) = split />/, $lema;
   # unless ($body) {say $lema; next;}    # For debugging only
-  $head =~ s/^\s+//;                    # Remove leading spaces
-  $head = lc($head);                    # Lower case headwords
+  $head =~ s/^\s+//;                     # Remove leading spaces
+  $head = lc($head);                     # Lower case headwords
   $head =~ s/[^\x{0370}-\x{03df}\x{1f00}-\x{1ffe}\s]//g;  # Remove non Greek symbols
   $body =~ s/\n?\s+/ /gs;                # Strip \n and multiple spaces
-  $body =~ s/\- //g;                     # Srtip hyphenation
+  $body =~ s/\- //g;                     # Strip hyphenation
   $body =~ s/^[:·]\s+//;
   $body =~ s/[\]\[]//g;
-  $body =~ s/\s+$//;                    # Remove trailling spaces from body
+  $body =~ s/\s+$//;                     # Remove trailing spaces from body
 
   # Print in DSL format
   say $head;
